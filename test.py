45cd2a2dec7f3c8a8dfa84b61abf783c35ca8fd6
Luke Hodkinson <furious.luke@gmail.com>
2020-05-21 09:53:10 +1000

    Dependency bump and minor test upgrade

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import os
+import sys
+import json
 
 from pycrime import CrimeClient
 
 
+try:
+    postcode = sys.argv[1]
+except IndexError:
+    print('Please provide a postcode')
+    sys.exit(1)
+
 jwt = os.environ.get('CRIME_API_KEY')
 client = CrimeClient(jwt=jwt)
-response = client.locality_statistics('3000')
-print(response)
+response = client.locality_statistics(postcode)
+print(json.dumps(response, indent=2))
